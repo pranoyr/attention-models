d3020a1105feb5c8c587e95950dd9ada3bc21c38
Pranoy <pranoyalkr@gmail.com>
2023-10-16 12:18:48 +0530

    init

--- transformer.py
+++ transformer.py
@@ -4,54 +4,67 @@ import math
 from einops import rearrange
 
 
+# n - number of timesteps
+# h - number of heads (num_heads)
+# d - dimension of each head (dim_head)
+# b - batch size
+
 class MultiHeadAttention(nn.Module):
-    def __init__(self, d_model, num_heads):
+    def __init__(self, d_model, num_heads, dim_head):
         super(MultiHeadAttention, self).__init__()
-        assert d_model % num_heads == 0, "d_model must be divisible by num_heads"
+
         
         self.d_model = d_model
         self.num_heads = num_heads
-        self.d_k = d_model // num_heads
+        self.dim_head = dim_head
         
-        self.W_q = nn.Linear(d_model, d_model)
-        self.W_k = nn.Linear(d_model, d_model)
-        self.W_v = nn.Linear(d_model, d_model)
-        self.W_o = nn.Linear(d_model, d_model)
+        self.W_q = nn.Linear(d_model, num_heads * dim_head)
+        self.W_k = nn.Linear(d_model, num_heads * dim_head)
+        self.W_v = nn.Linear(d_model, num_heads * dim_head)
+        self.W_o = nn.Linear(num_heads * dim_head, d_model)
         
-    def scaled_dot_product_attention(self, Q, K, V, mask=None):
-        attn_scores = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(self.d_k)
-        print(Q.shape)
-        print(K.transpose(-2, -1).shape)
+    def forward(self, Q, K, V, mask=None): 
+
+        # prepare Q, K, V for attention
+        # Q,K,V - (b_size, n_timesteps, n_heads * dk)
+        Q = self.W_q(Q)    
+        K = self.W_k(K)  
+        V = self.W_v(V)
+        
+        # split Q, K, V into multiple heads
+        # (b, n, h * d) -> (b, h, n, d)
+        Q = rearrange(Q, 'b n (h d) -> b h n d', h=self.num_heads)
+        K = rearrange(K, 'b n (h d) -> b h n d', h=self.num_heads)
+        V = rearrange(V, 'b n (h d) -> b h n d', h=self.num_heads)
+
+        # compute attention scores
+        #  (b, h, n, d) * (b, h, d, n) -> (b, h, n, n)
+        attn_scores = torch.matmul(Q, rearrange(K, 'b h n d -> b h d n')) / math.sqrt(self.dim_head)
+        
+        # apply mask
         print(attn_scores.shape)
         if mask is not None:
             attn_scores = attn_scores.masked_fill(mask == 0, -1e9)
         attn_probs = torch.softmax(attn_scores, dim=-1)
-        output = torch.matmul(attn_probs, V)
-        print(V.shape)
-        return output
-        
-    def split_heads(self, x):
-        batch_size, seq_length, d_model = x.size()
-        # x - (batch_size, seq_length, d_model)
-        return x.view(batch_size, seq_length, self.num_heads, self.d_k).transpose(1, 2)
-        
-    def combine_heads(self, x):
-        batch_size, _, seq_length, d_k = x.size()
-        x = rearrange(x, 'b h n d -> b n (h d)')
-        return x
-        
-    def forward(self, Q, K, V, mask=None):
-        Q = self.split_heads(self.W_q(Q)) # Q -(batch_size, num_heads, seq_length, d_k)
-        K = self.split_heads(self.W_k(K)) # K -(batch_size, num_heads, seq_length, d_k)
-        V = self.split_heads(self.W_v(V))   # V -(batch_size, num_heads, seq_length, d_k)
-        
-        attn_output = self.scaled_dot_product_attention(Q, K, V, mask)
-        output = self.W_o(self.combine_heads(attn_output))
-        return output
 
-attention = MultiHeadAttention(512, 8)
-Q = torch.randn(2, 10, 512)
-K = torch.randn(2, 10, 512)
-V = torch.randn(2, 10, 512)
-output = attention(Q, K, V)
-# print(output.shape) 
+        # Apply attention scores to V
+        # (b, h, n, n) * (b, h, n, d) -> (b, h, n, d)
+        output = torch.matmul(attn_probs, V)
+
+        # combine heads
+        output = rearrange(output, 'b h n d -> b n (h d)')
+        output = self.W_o(output)
+        return output
+    
+
+
+attention = MultiHeadAttention(d_model=512, num_heads=16, dim_head=64)
+Q = torch.randn(2, 10, 512) # (b, n, d_model)
+K = torch.randn(2, 20, 512)
+V = torch.randn(2, 20, 512)
+
+mask = torch.zeros(2, 16, 10, 20) # (b, h, timesteps_q, timesteps_k)
+
+output = attention(Q, K, V, mask)
+print(output.shape)
+
