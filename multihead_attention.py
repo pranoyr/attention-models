be35c8ea0f4d8cf231743fe0735fc7bc5f287017
Pranoy <pranoyalkr@gmail.com>
2023-10-16 14:26:24 +0530

    refactored

--- multihead_attention.py
+++ multihead_attention.py
@@ -13,24 +13,23 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, d_model, num_heads, dim_head):
         super(MultiHeadAttention, self).__init__()
 
-        
         self.d_model = d_model
         self.num_heads = num_heads
         self.dim_head = dim_head
-        
+
         self.W_q = nn.Linear(d_model, num_heads * dim_head)
         self.W_k = nn.Linear(d_model, num_heads * dim_head)
         self.W_v = nn.Linear(d_model, num_heads * dim_head)
         self.W_o = nn.Linear(num_heads * dim_head, d_model)
-        
-    def forward(self, Q, K, V, mask=None): 
+
+    def forward(self, Q, K, V, mask=None):
 
         # prepare Q, K, V for attention
         # Q,K,V - (b_size, n_timesteps, n_heads * dk)
-        Q = self.W_q(Q)    
-        K = self.W_k(K)  
+        Q = self.W_q(Q)
+        K = self.W_k(K)
         V = self.W_v(V)
-        
+
         # split Q, K, V into multiple heads
         # (b, n, h * d) -> (b, h, n, d)
         Q = rearrange(Q, 'b n (h d) -> b h n d', h=self.num_heads)
@@ -38,33 +37,32 @@ class MultiHeadAttention(nn.Module):
         V = rearrange(V, 'b n (h d) -> b h n d', h=self.num_heads)
 
         # compute attention scores
-        #  (b, h, n, d) * (b, h, d, n) -> (b, h, n, n)
+        #  Q(b, h, n, d) * K(b, h, d, n) ->  (b, h, n, n)
         attn_scores = torch.matmul(Q, rearrange(K, 'b h n d -> b h d n')) / math.sqrt(self.dim_head)
-        
+
         # apply mask
         if mask is not None:
             attn_scores = attn_scores.masked_fill(mask == 0, -1e9)
         attn_probs = torch.softmax(attn_scores, dim=-1)
 
         # Apply attention scores to V
-        # (b, h, n, n) * (b, h, n, d) -> (b, h, n, d)
+        # (b, h, n, n) * V(b, h, n, d) -> (b, h, n, d)
         output = torch.matmul(attn_probs, V)
 
         # combine heads
         output = rearrange(output, 'b h n d -> b n (h d)')
         output = self.W_o(output)
         return output
-    
+
 
 if __name__ == '__main__':
-    
-    attention = MultiHeadAttention(d_model=512, num_heads=16, dim_head=64)
-    Q = torch.randn(2, 10, 512) # (b, timesteps_q, d_model)
-    K = torch.randn(2, 20, 512) # (b, timesteps_k, d_model)
-    V = torch.randn(2, 20, 512) # (b, timesteps_v, d_model)
 
-    mask = torch.zeros(2, 16, 10, 20) # (b, h, timesteps_q, timesteps_k)
+    attention = MultiHeadAttention(d_model=512, num_heads=16, dim_head=64)
+    Q = torch.randn(2, 10, 512)  # (b, timesteps_q, d_model)
+    K = torch.randn(2, 20, 512)  # (b, timesteps_k, d_model)
+    V = torch.randn(2, 20, 512)  # (b, timesteps_v, d_model)
+
+    mask = torch.zeros(2, 16, 10, 20)  # (b, h, timesteps_q, timesteps_k)
 
     output = attention(Q, K, V, mask)
     print(output.shape)
-
