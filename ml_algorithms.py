da33fd9cc2d43d1c26cfeeebb81f4c192e4e1f90
Pranoy <pranoyalkr@gmail.com>
2023-10-19 12:45:50 +0530

    added linear regression

--- ml_algorithms.py
+++ ml_algorithms.py
@@ -13,15 +13,14 @@ num_features = 5
 x , y = torch.randn(100, num_features), torch.randn(100, 1)
 
 
-# Y  = XW^T + b   or  Y = x1w1 + x2w2 + x3w3 + x4w4 + x5w5 + b
 # make W and b trainable parameters
-
 # W must contains as many paramters as there are features in X
 W = torch.randn(num_features, requires_grad=True)
 b = torch.randn(1, requires_grad=True)
 
 W_t = rearrange(W, 'i -> i 1')
 
+# Y  = XW^T + b   or  Y = x1w1 + x2w2 + x3w3 + x4w4 + x5w5 + b
 # (b, n_feat) * (n_feat, 1) -> (b, 1)
 y_pred = einsum('b i, i j -> b j', x, W_t) + b
 
