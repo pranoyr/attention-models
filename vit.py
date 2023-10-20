a1ab0a87066c24c38550eea723231147f43ca1bd
Pranoy <pranoyalkr@gmail.com>
2023-10-21 01:24:48 +0530

    updated vit.py

--- vit.py
+++ vit.py
@@ -1,35 +1,43 @@
 import torch
 import torch.nn as nn
-from einops import rearrange, reduce, repeat
+from einops import rearrange, reduce, repeat, pack
 from torch import einsum
 import cv2
+from positional_encoding import AbsolutePositionalEmbedding
 
-
-img_batch = torch.randn(2, 3, 256, 256)
-
-img = cv2.imread('/home/pranoy/Downloads/foundation.jpg')
-img = cv2.resize(img, (256, 256))
-img = img.transpose(2, 0, 1)
-img = torch.from_numpy(img).unsqueeze(0).float()
-# (batch_size, channels, height, width) --> (batch_size, timesteps, features)
-
-
-def pair (t):
-    return t if isinstance(t, tuple) else (t, t)
 
 
 class Vit(nn.Module):
     def __init__(self, patch_size = 64):
         super(Vit, self).__init__()
-        self.patch_size = pair(patch_size)
+        
+        self.patch_size = patch_size
+
+        # number of features inside a patch
+        self.features = patch_size * patch_size * 3
+
+        self.class_token = nn.Parameter(torch.randn(1, self.features))
+        self.pos_enc =  AbsolutePositionalEmbedding(self.features, max_len=1000)
+
 
     def forward(self, x):
-        x = rearrange(x, 'b c (h p1) (w p2)  -> b (h w) p1 p2 c', h=64, w=64)
+        # (batch_size, channels, height, width) --> (batch_size, timesteps, features)
+        x = rearrange(x, 'b c (h p1) (w p2)  -> b (h w) (p1 p2 c)', p1=self.patch_size, p2=self.patch_size)
+
+        # add class token
+        class_token = repeat(self.class_token, 't d -> b t d', b=x.shape[0])
+        x = pack([class_token, x], "b * d")[0]
+        
+        # add absolute position embedding
+        # x = self.pos_enc(x)
+        # transformer
         return x
     
-# model = Vit()
-# out = model(img)
-# print(out.shape)
+model = Vit()
+
+img_batch = torch.randn(2, 3, 256, 256)
+out = model(img_batch)
+print(out.shape)
 
 # patch = out[0][0]    
 # print(torch.sum(patch))
