a851d21bcb6393fa3882fb3a6995415863fe47e2
Pranoy <pranoy@Pranoys-MacBook-Air.local>
2024-01-04 10:17:34 +0530

    updated logging

--- main.py
+++ main.py
@@ -6,21 +6,16 @@ from trainers import build_trainer
 import logging
 
 
-def select_log_level(level):
+def select_log_level(cfg):
     """ Selects the log level based on the input string
     """
-    if level == "debug":
-        level = logging.DEBUG
-    elif level == "info":
-        level = logging.INFO
-    elif level == "warning":
-        level = logging.WARNING
-    elif level == "error":
-        level = logging.ERROR
-    elif level == "critical":
-        level = logging.CRITICAL
-
-    logging.basicConfig(level=level)
+    levels = {
+        "debug": logging.DEBUG,
+        "info": logging.INFO,
+        "warning": logging.WARNING,
+        "error": logging.ERROR,
+    }
+    logging.basicConfig(level=levels[cfg.experiment.log_level])
 
 
 def get_config():
@@ -33,13 +28,12 @@ def get_config():
 	return conf
 
 
-
 if __name__=="__main__":
     
     cfg = get_config()
     
     # log level
-    select_log_level(cfg.experiment.log_level)
+    select_log_level(cfg)
     
     # build the model, data loader and trainer
     model = build_model(cfg)
@@ -48,7 +42,3 @@ if __name__=="__main__":
     
     # train the model
     trainer.train()
-    
-    
-  
-    
