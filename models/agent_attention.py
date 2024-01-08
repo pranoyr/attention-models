5e33884f2888551ec510f4b69fef2c372940df80
Pranoy <pranoyalkr@gmail.com>
2024-01-08 18:37:16 +0530

    based agent attention

--- models/agent_attention.py
+++ models/agent_attention.py
@@ -14,70 +14,67 @@ from torch import einsum
 # i - number of timesteps for Q
 # j - number of timesteps for K
 
+
+
 # helper function
 def exists(val):
 	return val is not None
 
-class AtentAttention(nn.Module):
-	def __init__(self, dim, num_heads=8, dim_head=64, dropout=0.0):
-		super(AtentAttention, self).__init__()
+
+class AgentAttention(nn.Module):
+	def __init__(self, dim, num_heads=8, dim_head=64, agent_num=47, dropout=0.0):
+		super(AgentAttention, self).__init__()
 
 		self.dim = dim
 		self.num_heads = num_heads
 		self.dim_head = dim_head
+  
+		inner_dimension = num_heads * dim_head
 
-		self.qkv = nn.Linear(dim, dim * 3, bias=False)
+		self.qkv = nn.Linear(dim, 3 * inner_dimension , bias=False)
 		self.scale = dim_head ** -0.5
+
+		pool_size = int(agent_num ** 0.5)
+		self.pool = nn.AdaptiveAvgPool2d((pool_size, dim_head))
+  
+		self.W_o = nn.Linear(num_heads * dim_head, dim)
+		self.dropout = nn.Dropout(dropout)
 
 	def forward(self, x, causal_mask=None, context_mask=None):
 		
 		qkv = self.qkv(x)
-		print(qkv.shape)
 
-		qkv = self.qkv(x).reshape(b, n, 3, c).permute(2, 0, 1, 3)
-        q, k, v = qkv[0], qkv[1], qkv[2]  # make torchscript happy (cannot use tensor as tuple)
-
-		# prepare Q, K, V for attention
-		# Q,K,V - (b_size, n_timesteps, n_heads * dk)
-		# q = self.W_q(q)
-		# k = self.W_k(k)
-		# v = self.W_v(v)
-
-		# # split Q, K, V into multiple heads
-		# # (b, t, h * d) -> (b, h, t, d)
-		# q = rearrange(q, 'b t (h d) -> b h t d', h=self.num_heads)
-		# k = rearrange(k, 'b t (h d) -> b h t d', h=self.num_heads)
-		# v = rearrange(v, 'b t (h d) -> b h t d', h=self.num_heads)
-
-		# # compute attention scores
-		# # Attention Scores = Q * K^T / sqrt(d_k)
-		# #  Q(b, h, t, d) * K(b, h, d, t) ->  (b, h, t, t)
-		# k_transpose = rearrange(k, 'b h t d -> b h d t')
-		# attn_scores = einsum('b h i d, b h d j -> b h i j', q * self.scale, k_transpose)
+		qkv = rearrange(qkv, 'b t (qkv h d) -> qkv b h t d', d = self.dim_head, h = self.num_heads)
+		q , k , v = qkv[0], qkv[1], qkv[2]
 		
-		# # context mask used in Cross-Attention (encoder-decoder) and Self-Attention (encoder)
-		# if exists(context_mask):
-		# 	context_mask = rearrange(context_mask, 'b j -> b 1 1 j')
-		# 	attn_scores = attn_scores.masked_fill(~context_mask, -1e9)
+		# pooling the queries to get agent tokens
+		agent_tokens = self.pool(q)
+  
+		# Agent Aggregation
 
-		# # causal mask used in Masked Multi-Head Attention (decoder)
-		# if exists(causal_mask):
-		# 	attn_scores = attn_scores.masked_fill(causal_mask, -1e9)
-		# attn_probs = torch.softmax(attn_scores, dim=-1)
+		attn_scores = einsum('b h i d, b h j d -> b h i j', agent_tokens * self.scale, k)
+  
+		if exists(causal_mask):
+			attn_scores = attn_scores.masked_fill(causal_mask, -1e9)
+   
+		attn_probs = torch.softmax(attn_scores, dim=-1)
+   
+		v_agent = einsum('b h i j, b h j d -> b h i d', attn_probs, v)
+  
 
-		# # Apply attention scores to V
-		# # (b, h, t, t) * V(b, h, t, d) -> (b, h, t, d)
-		# output = einsum('b h i j, b h j d -> b h i d', attn_probs, v)
+		# Agent Broadcast
 
-		# # combine heads
-		# output = rearrange(output, 'b h t d -> b t (h d)')
-		# output = self.W_o(output)
-		# output = self.dropout(output)
-		# return output
-	
-# b , time, dim 
-x = torch.randn(1, 16, 256)
+		attn_scores = einsum('b h i d, b h j d -> b h i j', q * self.scale, agent_tokens)
+  
+		if exists(causal_mask):
+			attn_scores = attn_scores.masked_fill(causal_mask, -1e9)
+		attn_probs = torch.softmax(attn_scores, dim=-1)
 
-attn = AtentAttention(256)
-
-out = attn(x)
+		output = einsum('b h i j, b h j d -> b h i d', attn_probs, v_agent)
+  
+		# combine 
+		output = rearrange(output, 'b h t d -> b t (h d)')
+		output = self.W_o(output)
+		output = self.dropout(output)
+   
+		return output
