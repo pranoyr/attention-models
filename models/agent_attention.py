2d46eb7f95770e3773f12de95a88da4e11ac1f7a
Pranoy <pranoy@Pranoys-MacBook-Air.local>
2024-01-11 01:29:21 +0530

    added TODO

--- models/agent_attention.py
+++ models/agent_attention.py
@@ -13,11 +13,9 @@ import torch.nn.functional as F
 # t - number of timesteps of Q,K,V
 
 
-
 # helper function
 def exists(val):
 	return val is not None
-
 
 
 class AgentAttention(nn.Module):
@@ -38,6 +36,8 @@ class AgentAttention(nn.Module):
   
 		self.W_o = nn.Linear(num_heads * dim_head, dim)
 		self.dropout = nn.Dropout(dropout)
+
+		# TODO  - replace bias with correct implementation
 
 		self.bias1 = nn.Parameter(torch.zeros(1, 1, 1, 1))
 		self.bias2 = nn.Parameter(torch.zeros(1, 1, 1, 1))
