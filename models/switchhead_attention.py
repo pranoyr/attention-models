b7cea63803523fa88c16af50890209a09af0bc47
Pranoy <pranoy@Pranoys-MacBook-Air.local>
2024-01-16 19:08:02 +0530

    switchhead compelte

--- models/switchhead_attention.py
+++ models/switchhead_attention.py
@@ -12,31 +12,32 @@ def exists(val):
 
 
 
+
 class SwitchHeadAttention(nn.Module):
-	def __init__(self, dim, num_heads=8, dim_head=64, dropout=0.0):
+	def __init__(self, dim, num_heads=8, dim_head=64, num_experts=5, dropout=0.0):
 		super(SwitchHeadAttention, self).__init__()
 
 		self.dim = dim
 		self.num_heads = num_heads
 		self.dim_head = dim_head
+		self.num_experts = num_experts
 
-
-		self.q = nn.Linear(dim, num_heads * dim_head, bias=False)
 
 		self.q = nn.Sequential(
-			nn.Linear(dim, num_heads * dim_head, bias=False),
+			nn.Linear(dim, num_heads * num_experts * dim_head, bias=False),
 			nn.Dropout(dropout),
-			Rearrange('b t (h d) -> b h t d', h=self.num_heads)
+			Rearrange('b t (h e d) -> b t h e d', h=self.num_heads, e=self.num_experts)
 		)
 
 		self.kv = nn.Sequential(
-			nn.Linear(dim, 2 * num_heads * dim_head, bias=False),
+			nn.Linear(dim, 2 * num_heads * num_experts * dim_head, bias=False),
 			nn.Dropout(dropout),
-			Rearrange('b t (kv h d) -> kv b h t d', d = self.dim_head, h = self.num_heads)
+			Rearrange('b t (kv h e d) -> kv b t h e d', d = self.dim_head, h = self.num_heads, e=self.num_experts)
 		)
 
 
-		self.W_s = nn.Linear(dim, num_heads, bias=False)
+		self.W_s = nn.Linear(dim, num_heads * num_experts, bias=False)
+		self.W_d = nn.Linear(dim, num_heads * num_experts, bias=False)
 	
 		self.W_o = nn.ModuleList([nn.Linear(dim_head, dim) for _ in range(num_heads)])
 
@@ -44,27 +45,47 @@ class SwitchHeadAttention(nn.Module):
 
 		self.scale = dim_head ** -0.5
 
+	def get_scores(self, eps, s):
+		scores = torch.zeros_like(s)
+		scores.scatter_(3, eps, s)
+		scores = repeat(scores, 'b t h e -> b t h e d', d = self.dim_head)
+		return scores
+
+
 	def forward(self, x, context=None, causal_mask=None, context_mask=None):
 
-		s = torch.sigmoid(self.W_s(x))
+		ss = torch.sigmoid(self.W_s(x))
+		sd = torch.sigmoid(self.W_d(x))
 
-		eps = s.topk(k=3, dim=2).indices
+		ss = rearrange(ss, 'b t (h e) -> b t h e', h=self.num_heads)
+		sd = rearrange(sd, 'b t (h e) -> b t h e', h=self.num_heads)
+		
+		eps_s = ss.topk(k=3, dim=3).indices
+		eps_d = sd.topk(k=3, dim=3).indices
 
-		scores = torch.zeros_like(s)
-		scores.scatter_(2, eps, s)
-		scores = repeat(scores, 'b t s -> b t s d', d = self.dim)
-		scores = rearrange(scores, 'b t s d -> s b t d')
+		scores_q = self.get_scores(eps_d , sd)
+		scores_k = self.get_scores(eps_s , ss)
 
-		q = self.q(x)
+	
+		q = self.q(x) * scores_q  # b, h, t, 
+		q = q.sum(dim=-2)
+		q = rearrange(q, 'b t h d -> b h t d')
+
 
 		if exists(context):
 			k, v = self.kv(context)
 		else:
 			k, v = self.kv(x)
-		
+
+		k = k * scores_k
+		k = k.sum(dim=-2)
+		k = rearrange(k, 'b t h d -> b h t d')
+
+		v = v * scores_k
+		v = v.sum(dim=-2)
+		v = rearrange(v, 'b t h d -> b h t d')
+
 		# compute attention scores
-		# Attention Scores = Q * K^T / sqrt(d_k)
-		#  Q(b, h, t, d) * K(b, h, d, t) ->  (b, h, t, t)
 		attn_scores = einsum('b h i d, b h d j -> b h i j', q * self.scale, k.transpose(-1, -2))
 		
 		# context mask used in Cross-Attention (encoder-decoder) and Self-Attention (encoder)
@@ -78,12 +99,11 @@ class SwitchHeadAttention(nn.Module):
 		attn_probs = torch.softmax(attn_scores, dim=-1)
 
 		# Apply attention scores to V
-		# (b, h, t, t) * V(b, h, t, d) -> (b, h, t, d)
 		output = einsum('b h i j, b h j d -> b h i d', attn_probs, v)
 		output = rearrange(output, 'b h t d -> h b t d')
 
 
-		output = [w(o_head) * s for w, o_head , s  in zip(self.W_o, output, scores)]
+		output = [w(o_head)  for w, o_head   in zip(self.W_o, output)]
 
 		output = torch.stack(output, dim=2).sum(dim=2)
 	
