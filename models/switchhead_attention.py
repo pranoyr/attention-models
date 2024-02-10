45af614e60ace7ff8d70f2336e1a6f3092695896
Pranoy <pranoy@Pranoys-MacBook-Air.local>
2024-02-10 22:41:24 +0530

    switchhead simplified

--- models/switchhead_attention.py
+++ models/switchhead_attention.py
@@ -38,33 +38,33 @@ class SwitchHeadAttention(nn.Module):
 			Rearrange('b t (h d) -> b h t d', h=self.num_heads)
 		)
 
-		self.experts_v = nn.ModuleList([nn.Linear(dim, dim_head, bias=False) for _ in range(num_experts)])
-
 		self.W_s = nn.Sequential(
 			nn.Linear(dim, num_heads * num_experts, bias=False),
 			Rearrange('b t (h e) -> b t h e', h=self.num_heads, e=self.num_experts)
 		)
+
+		self.experts_v = nn.ModuleList([nn.Linear(dim, dim_head, bias=False) for _ in range(num_experts)])
 
 		self.W_d =  nn.Sequential(
 			nn.Linear(dim, num_heads * num_experts, bias=False),
 			Rearrange('b t (h e) -> b t h e', h=self.num_heads, e=self.num_experts)
 		)
 	
-		self.experts_o = nn.ModuleList([nn.Linear(dim_head, dim, bias=False) for _ in range(num_experts)])
+		self.experts_out = nn.ModuleList([nn.Linear(dim_head, dim, bias=False) for _ in range(num_experts)])
 
 		self.scale = dim_head ** -0.5
+		self.inf = -1e9
 
+	def moe_v(self, inputs):
+		# inputs shape - (b, t, d)
+		b , t = inputs.shape[:2]
 
-	def compute_moe(self, inputs, gate, experts):
-		# input shape - (b, t, d)
-		b , t, _ = inputs.shape
-		gate_logits = gate(inputs) 
+		gate_logits = self.W_s(inputs) 
 		weights, selected_experts = torch.topk(gate_logits, self.sel_experts)
 		weights = torch.sigmoid(weights).to(inputs.dtype)
 
-		# results should of shape - (b, t  h, d)
 		results = torch.zeros(b, t, self.num_heads, self.dim_head, device=inputs.device)
-		for i, expert in enumerate(experts):
+		for i, expert in enumerate(self.experts_v):
 			batch_idx, t, h, nth_expert = torch.where(selected_experts == i)
 			results[batch_idx, t, h] += weights[batch_idx, t, h, nth_expert, None] * expert(
 				inputs[batch_idx, t]
@@ -72,34 +72,27 @@ class SwitchHeadAttention(nn.Module):
 		results = rearrange(results, 'b t h d -> b h t d')
 		return results
 	
+	def moe_out(self, inputs, gate_inputs):
+		# inputs shape - (b, t, h, d)
+		# gate_inputs shape - (b, t, d)
+		b , t = gate_inputs.shape[:2]
 
-	def compute_moe_o(self, inputs, gate_in, gate, experts):
-		# gatein shape- (b, t, d)
-		# input shape - (b, t, h, d)
-		b , t , _ = gate_in.shape
-
-		gate_logits = gate(gate_in) 
+		gate_logits = self.W_d(gate_inputs) 
 		weights, selected_experts = torch.topk(gate_logits, self.sel_experts)
 
-		# results should of shape - (b, t  h, d)
 		results = torch.zeros(b, t, self.num_heads, self.dim, device=inputs.device)
-		for i, expert in enumerate(experts):
+		for i, expert in enumerate(self.experts_out):
 			batch_idx, t, h, nth_expert = torch.where(selected_experts == i)
 			results[batch_idx, t, h] += expert(inputs[batch_idx, t, h]
 			)
 		return results
 	
-	
 	def forward(self, x, context=None, causal_mask=None, context_mask=None):
-
-	
 		# prepare query, key, value
 		q = self.q(x) 
 		x = default(context, x)
 		k = self.k(x)
-		
-		
-		v = self.compute_moe(x, gate=self.W_s, experts=self.experts_v)
+		v = self.moe_v(x)
 	
 		# compute attention scores
 		attn_scores = einsum('b h i d, b h d j -> b h i j', q * self.scale, k.transpose(-1, -2))
@@ -107,20 +100,17 @@ class SwitchHeadAttention(nn.Module):
 		# context mask used in Cross-Attention (encoder-decoder) and Self-Attention (encoder)
 		if exists(context_mask):
 			context_mask = rearrange(context_mask, 'b j -> b 1 1 j')
-			attn_scores = attn_scores.masked_fill(~context_mask, -1e9)
+			attn_scores = attn_scores.masked_fill(~context_mask, self.inf)
 
 		# causal mask used in Masked Multi-Head Attention (decoder)
 		if exists(causal_mask):
-			attn_scores = attn_scores.masked_fill(causal_mask, -1e9)
+			attn_scores = attn_scores.masked_fill(causal_mask, self.inf)
 		attn_probs = torch.softmax(attn_scores, dim=-1)
 
 		# Apply attention scores to V
 		output = einsum('b h i j, b h j d -> b i h d', attn_probs, v)
-
-		# output = torch.einsum('b t h e , b t h e d -> b t h d', sd, self.W_o(output))
-		output = self.compute_moe_o(output, x, gate=self.W_d, experts=self.experts_o)
+		output = self.moe_out(output, gate_inputs=x)
 
 		# sum over heads
 		output = output.sum(dim=-2)
 		return output
-	
